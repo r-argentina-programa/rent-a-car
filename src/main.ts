3c641e5d4f9cca779de8668a72acd1521b69a482
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2021-03-20 17:25:24 -0300

    Resuelve mensaje de eslint shadowing

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/app.module';
 
-const port = Number(process.env.PORT) || 3000;
-
 async function bootstrap(port: number) {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }
 
+const port = Number(process.env.PORT) || 3000;
 bootstrap(port).then(() => process.stdout.write(`Bootstrap complete: http://localhost:${port}`));

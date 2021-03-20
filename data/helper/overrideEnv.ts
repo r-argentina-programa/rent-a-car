cba11b15935f0227fe25e663a5968e260ca53666
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2021-03-20 18:24:58 -0300

    Elimina helper sin uso

--- data/helper/overrideEnv.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import dotenv from 'dotenv';
-
-export function overrideEnv(envFilePath) {
-  const envConfig = dotenv.parse(fs.readFileSync(envFilePath));
-  Object.keys(envConfig).forEach((k) => {
-    process.env[k] = envConfig[k];
-  });
-}

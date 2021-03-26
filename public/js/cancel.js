0e16254af5762b542490cb65fe83ed8fd86970ec
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2021-03-26 22:39:21 +1100

    Elimina carpeta public sin uso en la API

--- public/js/cancel.js
+++
@@ -1,5 +0,0 @@
-function cancelForm() {
-  window.history.back();
-}
-
-document.querySelector('.cancel-button').addEventListener('click', cancelForm);
--- public/js/toggle-menu.js
+++
@@ -1,9 +0,0 @@
-const $navbarBurger = document.querySelector('.navbar-burger');
-
-function toggleMenu() {
-  const $navbarMenu = document.querySelector('.navbar-menu');
-  $navbarBurger.classList.toggle('is-active');
-  $navbarMenu.classList.toggle('is-active');
-}
-
-$navbarBurger.addEventListener('click', toggleMenu);
